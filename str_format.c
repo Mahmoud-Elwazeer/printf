31aeec3c1e135976a3ca9e86f7035efa9d6d4f5a
Karim-Moharm <karimmoharm222@gmail.com>
2023-04-22 21:30:08 -0700

    updating check_specifier file

--- str_format.c
+++ str_format.c
@@ -1,19 +1,3 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 int s_format(char *string)
-{
-	int i = 0;
-	int count = 0;
-
-	if(string)
-	{
-		while(string[i] != '\0')
-		{
-			_putchar(string[i]);
-			count++;
-
-		}
-	}
-}
