8ff2a040558e1d8a3bd54ce4471c8ec72438cfe5
Karim-Moharm <karimmoharm222@gmail.com>
2023-04-22 20:19:43 -0700

    adding new file

---
+++ check_specifier.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * check_specifier - check for the format specifier in printf function
+  * @format: string 
+  * Return: function pointer
+  */
+
+int (*check_specifier(char *format))(va_list)
+/* int (*va_list) (check_specifier)(char* format) */
+{
+	
+
+
+}
