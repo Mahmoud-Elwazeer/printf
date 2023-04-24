77dd94c04118e62eb6c66ee0ca3193957f3c4bb5
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-23 17:10:04 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -45,8 +45,8 @@ int _printf(const char *format, ...)
 						size += binary(va_arg(args, unsigned int));
 						break;
 					case 'r':
-						size++;
-						_putchar('r');
+						size += _strlen(va_arg(args, char *));
+						reverse_string(va_arg(args, char*));
 						break;
 					default:
 						break;
